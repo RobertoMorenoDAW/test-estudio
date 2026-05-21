686350f393a8d81bdfa3f56f2c836625a3b35805
Roberto Moreno Moreno <robertodawdam@gmail.com>
2026-05-21 12:58:30 +0200

    21/05/2026 12:58:30,62

--- subir.bat
+++ subir.bat
@@ -1,13 +1,53 @@
 @echo off
 
+echo =====================
+echo ESTADO GIT
+echo =====================
 git status
-git add .
-git commit -m "%date% %time%"
-git push
 
 echo.
 echo =====================
-echo PROCESO TERMINADO
+echo AÑADIENDO CAMBIOS
 echo =====================
+git add .
+
+echo.
+echo =====================
+echo CREANDO COMMIT
+echo =====================
+git commit -m "%date% %time%"
+
+echo.
+echo =====================
+echo SUBIENDO A GITHUB
+echo =====================
+
+set RETRY=0
+
+:PUSH
+git push
+
+IF %ERRORLEVEL% EQU 0 GOTO SUCCESS
+
+set /A RETRY=%RETRY%+1
+
+echo.
+echo Error de conexion. Reintentando... Intento %RETRY%
+timeout /t 5 >nul
+
+IF %RETRY% LSS 5 GOTO PUSH
+
+echo.
+echo ==================================
+echo NO SE PUDO SUBIR A GITHUB
+echo ==================================
+pause
+exit
+
+:SUCCESS
+echo.
+echo ==================================
+echo SUBIDA COMPLETADA CORRECTAMENTE
+echo ==================================
 
 pause
